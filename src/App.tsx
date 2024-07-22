be2da37794f0b02260e444b474b2cefe60769f09
Teodor P. <teopintilie3@gmail.com>
2024-07-23 02:39:07 +0300

    initial

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="bg-base-300 size-full overflow-auto">
+    <h1 className="text-3xl font-bold mt-5">
+      Hello world!
+    </h1>
+    <button className="btn btn-primary mt-5">Button</button>
+    </div>
+  )
+}
+
+export default App

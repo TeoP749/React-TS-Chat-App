e46d996fead6e960c2c604c8f1266bf5faa4ae2c
Teodor P. <teopintilie3@gmail.com>
2024-08-10 18:50:37 +0300

    username selector component

---
+++ src/components/users/UsernameSelection.tsx
@@ -0,0 +1,24 @@
+function UsernameSelection(props: { handleUsernameSelection: (username: string) => void }) {
+    const { handleUsernameSelection } = props;
+    return (
+        <div className="card bg-base-100 w-96 shadow-xl">
+            <div className="card-body flex items-center">
+                <h2 className="card-title">Welcome!</h2>
+                <p>Please select a username</p>
+                <input type="text" placeholder="John Doe" className="input input-bordered w-full max-w-xs" onKeyDown={(e) => {
+                    if (e.key === 'Enter') {
+                        const target: HTMLInputElement = e.target as HTMLInputElement;
+                        if (target.value !== null && target.value.trim() !== "") {
+                            handleUsernameSelection((e.target as HTMLInputElement).value);
+                        } else {
+                            alert("empty username");
+                        }
+                        target.value = "";
+                    }
+                }} />
+            </div>
+        </div>
+    );
+}
+
+export default UsernameSelection;

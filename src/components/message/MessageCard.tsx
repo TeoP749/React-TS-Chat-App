6968d6b6c00951934563db3ac9a54519b9a25c10
Teodor P. <teopintilie3@gmail.com>
2024-08-11 08:17:48 +0300

    styling for message card

--- src/components/message/MessageCard.tsx
+++ src/components/message/MessageCard.tsx
@@ -1,8 +1,9 @@
 import { useLayoutEffect, useRef, useState } from "react";
+import { User } from "../../App";
 
 export type Message = {
     id: string;
-    user: string;
+    user: User;
     message: string;
     timestamp?: number;
 }
@@ -30,7 +31,6 @@ const splitMessageIntoChunks = (message: string, maxLength: number) => {
 
 const format_time: (timestamp: number) => string = (timestamp: number) => {
     const date = new Date(timestamp);
-    //format HH/MM AM/PM
     const hours = date.getHours();
     const minutes = date.getMinutes();
     const ampm = hours >= 12 ? 'PM' : 'AM';
@@ -38,6 +38,7 @@ const format_time: (timestamp: number) => string = (timestamp: number) => {
 }
 
 function MessageCard(props: { message: Message, show_sender?: boolean }) {
+    const { message, show_sender } = props;
     const [contentHeight, setContentHeight] = useState(0);
     const contentRef = useRef<HTMLDivElement>(null);
     const message_chunks: string[] = splitMessageIntoChunks(props.message.message, 30);
@@ -47,20 +48,19 @@ function MessageCard(props: { message: Message, show_sender?: boolean }) {
             setContentHeight(contentRef.current.clientHeight);
         }
     }, [contentHeight]);
-    const { message, show_sender } = props;
-    const test: boolean = true;
+    const test: boolean = false;
     return (
         <>
             <div className="card rounded-lg bg-base-300 border-0 max-w-[20vw] w-fit shadow-md mt-0 mb-[0.1vh]">
                 {
-                    !test ?
+                    test ?
                         <></>
                         :
                         <div className="card-body flex flex-col p-2">
                             <div className="flex w-full items-start">
                                 <div ref={contentRef} className="flex flex-col items-start">
                                     {show_sender && (
-                                        <h2 className="text-primary card-compact py-0 m-0">{message.user}</h2>
+                                        <h2 className="text-primary card-compact py-0 m-0">{message.user.username}</h2>
                                     )}
                                     <div className="max-w-full text-left">
                                         {message_chunks.map((message_chunk) => (
@@ -80,7 +80,6 @@ function MessageCard(props: { message: Message, show_sender?: boolean }) {
                             </div>
                         </div>
                 }
-
             </div>
         </>
     )

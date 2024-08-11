e0d0fd9ab2c71e17f3adbc868ce65a186151b25a
Teodor P. <teopintilie3@gmail.com>
2024-08-11 08:19:05 +0300

    wrap message handler in a function

--- src/components/message_input/MessageInput.tsx
+++ src/components/message_input/MessageInput.tsx
@@ -3,20 +3,27 @@ import { faPaperPlane } from '@fortawesome/free-solid-svg-icons'
 import { useState } from 'react';
 import { Message } from '../message/MessageCard';
 import { v4 as uuidv4 } from 'uuid';
+import { User } from '../../App';
 
-function MessageInput(props: { onMessage: (message: Message) => void, user: string }) {
+function MessageInput(props: { onMessage: (message: Message) => void, user: User }) {
     const { onMessage, user } = props;
     const [message, setMessage] = useState("");
+    const handleMessage = (message: string) => {
+        if (message.trim() != "") {
+            onMessage({ id: uuidv4(), user: user, message: message, timestamp: Date.now() });
+            setMessage("");
+        }
+    }
+
     return (
         <div>
             <label className="input input-bordered flex items-center gap-2">
                 <input type="text" className="grow flex items-center justify-between" placeholder="message" value={message} onChange={(e) => setMessage(e.target.value)} onKeyDown={(e) => {
                     if (e.key === 'Enter') {
-                        onMessage({ id: uuidv4(), user: user, message: message, timestamp: Date.now() });
-                        setMessage("");
+                        handleMessage(message);
                     }
                 }} />
-                <FontAwesomeIcon icon={faPaperPlane} className="cursor-pointer" onClick={() => { onMessage({ id: uuidv4(), user: user, message: message }); setMessage("") }} />
+                <FontAwesomeIcon icon={faPaperPlane} className="cursor-pointer" onClick={() => handleMessage(message)} />
             </label>
         </div>
     );

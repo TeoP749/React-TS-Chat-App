634f35c4f84687a22c7b36d3a2118b5dd5f9e9ec
Teodor P. <teopintilie3@gmail.com>
2024-08-11 08:18:18 +0300

    user id check to message view

--- src/components/message/MessageView.tsx
+++ src/components/message/MessageView.tsx
@@ -1,42 +1,10 @@
 import { forwardRef } from 'react';
-import { Message } from './MessageCard';
-import MessageCard from './MessageCard';
+import { User } from '../../App';
+import MessageCard, { Message } from './MessageCard';
 import './MessageView.css';
-import { v4 as uuidv4 } from 'uuid';
-export const test_messages: Message[] = [
-    {
-        id: uuidv4(),
-        user: "John",
-        message: "Hello, how are you?",
-        timestamp: 1635730000000
-    },
-    {
-        id: uuidv4(),
-        user: "Jane",
-        message: "I'm good, thank you.",
-        timestamp: 1635730600000
-    },
-    {
-        id: uuidv4(),
-        user: "John",
-        message: "That's great to hear.",
-        timestamp: 1635731200000
-    }
-];
 
-export const get_test_message: () => Message = () => {
-    const users = ["John", "Jane", "Alice", "Bob"];
-    const messages = ["Hello", "How are you?", "I'm good, thank you.", "That's great to hear."]
-    const id: string = uuidv4();
-    const user: string = users[Math.floor(Math.random() * users.length)];
-    const message: string = messages[Math.floor(Math.random() * messages.length)] + " (this is a test message)";
-    const timestamp: number = Date.now();
-    return { id, user, message, timestamp };
-}
-
-
-const MessageView = forwardRef(function MessageView(props: { messages: Message[], current_user: string }, ref: React.ForwardedRef<HTMLDivElement>) {
-    const { messages, current_user } = props;
+const MessageView = forwardRef(function MessageView(props: { messages: Message[], selfUser: User }, ref: React.ForwardedRef<HTMLDivElement>) {
+    const { messages, selfUser } = props;
     let prev_message_sender: string = "";
     return (
         <div className="no-scrollbar size-full relative max-h-full overflow-y-auto pt-[1vh] pb-[1.5vh]" ref={ref}>
@@ -44,12 +12,12 @@ const MessageView = forwardRef(function MessageView(props: { messages: Message[]
                 {messages.map((message: Message) => {
 
                     const component = (
-                        <div key={message.id} className={message.user === current_user ? "self-end mr-[1vw]" : "self-start ml-[1vw]"}>
-                            <MessageCard message={message} show_sender={message.user !== current_user && message.user !== prev_message_sender} />
+                        <div key={message.id} className={message.user.userID === selfUser.userID ? "self-end mr-[1vw]" : "self-start ml-[1vw]"}>
+                            <MessageCard message={message} show_sender={message.user.userID !== selfUser.userID && message.user.userID !== prev_message_sender} />
                         </div>
                     );
 
-                    prev_message_sender = message.user;
+                    prev_message_sender = message.user.userID;
                     return component;
                 })}
             </div>

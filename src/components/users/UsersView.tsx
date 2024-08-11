a3460ccbb3cb4efde80ad497ecd95988e5d53591
Teodor P. <teopintilie3@gmail.com>
2024-08-11 08:16:10 +0300

    allow user selection

--- src/components/users/UsersView.tsx
+++ src/components/users/UsersView.tsx
@@ -1,20 +1,23 @@
 import { User } from "../../App";
 
-function UsersView(props: { users: User[], selectedUsername: string }) {
-    const { users, selectedUsername } = props;
+function UsersView(props: { users: User[], selfUser: User, selectedUser: User, setSelectedUser: (username: User) => void }) {
+    const { users, selfUser, selectedUser, setSelectedUser } = props;
     return (
         <div className="flex flex-col size-full bg-base-100">
             {
                 users.map((user, index) => {
-                    return (
-                        <div key={index} className={`${selectedUsername === user.username ? 'bg-primary' : ''}`}>
-                            <p>{user.username}</p>
-                        </div>
-                    );
+                    return user.userID !== selfUser.userID
+                        ?
+                        (
+                            <div key={index} className={`hover:cursor-pointer ${selectedUser.userID == user.userID ? 'bg-primary' : ''}`} onClick={() => { setSelectedUser(user) }}>
+                                <p>{user.username}</p>
+                            </div>
+                        )
+                        : <></>
                 })
             }
         </div>
     );
-}
+};
 
 export default UsersView;

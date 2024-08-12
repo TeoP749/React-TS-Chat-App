b1bf2ebb58ee34b8aa035e4fb6a29d81fb930e82
Teodor P. <teopintilie3@gmail.com>
2024-08-12 18:42:36 +0300

    use http server + find socket by user id

--- server/index.js
+++ server/index.js
@@ -1,14 +1,24 @@
 import { Server } from 'socket.io';
+import { createServer } from 'http';
 
 const RED = "\u001b[31m";
 const BLUE = "\u001b[34m";
 const RESET = "\u001b[0m";
 
-const io = new Server({
+const httpServer = createServer();
+const io = new Server(httpServer, {
   cors: {
     origin: "http://localhost:5173",
   }
 });
+
+const findSocket = (user_id) => {
+  for (let [id, socket] of io.of("/").sockets) {
+    if (socket.user_id === user_id) {
+      return id;
+    }
+  }
+}
 
 io.use((socket, next) => {
   const id = socket.handshake.auth.id;
@@ -41,8 +51,11 @@ io.on('connection', (socket) => {
     username: socket.username,
   });
 
-  socket.on('message', (msg) => {
-    socket.broadcast.emit('message', msg);
+  socket.on("private message", ({ message, to }) => {
+    socket.to(findSocket(to)).emit("private message", {
+      message,
+      from: socket.user_id,
+    });
   });
 
   socket.on("disconnect", () => {
@@ -63,4 +76,6 @@ setInterval(() => {
   console.log([...(io.of("/").sockets)].map((socket) => socket[1].username));
 }, 1000)
 
-io.listen(3000);
+httpServer.listen(3000, () => {
+  console.log("server listening at http://localhost:3000");
+});

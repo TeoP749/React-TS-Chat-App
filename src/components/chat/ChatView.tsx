73beaedb3a9a038fe2a3efb92d8e41595b7d2d07
Teodor P. <teopintilie3@gmail.com>
2024-08-13 15:51:37 +0300

    move message listeners to app

--- src/components/chat/ChatView.tsx
+++ src/components/chat/ChatView.tsx
@@ -1,30 +1,15 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useRef } from 'react';
 import { User } from '../../App';
-import { socket } from '../../socket';
 import { Message } from '../message/MessageCard';
 import MessageView from '../message/MessageView';
 import MessageInput from '../message_input/MessageInput';
 import './ChatView.css';
 
-function ChatView(props: { selected_user: User, selfUser: User, setUserMessages: (userID: string, updateCallback: (prevMessages: Message[] | undefined) => Message[]) => void }) {
-    const { selected_user, selfUser, setUserMessages } = props;
-    const [messages, setMessages] = useState<Message[]>(selected_user?.messages || []);
+export default function ChatView(props: { selected_user: User, messages: Message[], selfUser: User, onSelfMessage: (message: Message) => void }) {
+    console.log("RENDERING CHAT VIEW");
+    const { selected_user, messages, selfUser, onSelfMessage } = props;
+    console.log(messages.map((message) => message.message).join(','));
     const scrollElementRef: React.ForwardedRef<HTMLDivElement> = useRef(null);
-    const add_message: (message: Message) => void = (message: Message) => {
-        setUserMessages(selected_user.userID, (prevMessages: Message[] | undefined): Message[] => {
-            const newMessages = [...(prevMessages || []), message];
-            return newMessages;
-        });
-        setMessages(prevMessages => [...prevMessages, message]);
-    }
-
-    useEffect(() => {
-        const onMessage = (message: Message) => add_message(message);
-        socket.on('message', onMessage);
-        return () => {
-            socket.off('message', onMessage);
-        }
-    }, []);
 
     useEffect(() => {
         setTimeout(() => {
@@ -42,18 +27,11 @@ function ChatView(props: { selected_user: User, selfUser: User, setUserMessages:
                 selected_user.userID.trim() === "" ?
                     <></>
                     :
-                    <div className="bg-success-content size-full overflow-auto flex flex-col" >
+                    <div className="bg-base-300 size-full overflow-auto flex flex-col" >
                         <MessageView messages={messages} selfUser={selfUser} ref={scrollElementRef} />
                         <div className='mt-auto mx-[1vw] mb-[1vh] sticky flex items-center justify-center'>
                             <div className='w-full'>
-                                <MessageInput onMessage={
-                                    (message) => {
-                                        add_message(message);
-                                        socket.emit('message', message);
-                                    }
-                                }
-                                    user={selfUser}
-                                />
+                                <MessageInput onMessage={message => onSelfMessage(message)} selfUser={selfUser} />
                             </div>
                         </div>
                     </div >
@@ -61,6 +39,3 @@ function ChatView(props: { selected_user: User, selfUser: User, setUserMessages:
         </>
     )
 }
-
-
-export default ChatView

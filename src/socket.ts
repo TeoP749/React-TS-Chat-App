38f6f5e739c4dad7747d42599e10c42091ff6bdb
Teodor P. <teopintilie3@gmail.com>
2024-08-15 19:53:22 +0300

    add ws server url

--- src/socket.ts
+++ src/socket.ts
@@ -1,5 +1,6 @@
+import { env } from 'process';
 import { io } from 'socket.io-client';
 
-const URL: string = 'http://localhost:3000';
+const URL: string = env.WS_SERVER_URL || 'http://localhost:3000';
 
 export const socket = io(URL, { autoConnect: false });
